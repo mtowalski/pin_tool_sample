ce45d26bee3ca6f4b4d2a7df607fb277b0b0785a
mtowalski <mtowalski@users.noreply.github.com>
2016-09-02 17:43:06 +0200

    Fixes

--- download_pin.ps1
+++ download_pin.ps1
@@ -1,15 +1,17 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 $client = new-object System.Net.WebClient
-$zm=(Get-Item -Path ".\" -Verbose).FullName.toString()
-$fil=$zm+"\\pin-2.14-71313-msvc12-windows.zip"
-$res=$zm
-$client.DownloadFile("http://software.intel.com/sites/landingpage/pintool/downloads/pin-2.14-71313-msvc12-windows.zip",$zm+"\\pin-2.14-71313-msvc12-windows.zip")
 
-[System.IO.Compression.ZipFile]::ExtractToDirectory([string]$fil, [string]$res)
+$path=(Get-Item -Path ".\" -Verbose).FullName.toString()
+$file=$path+"\\pin-2.14-71313-msvc12-windows.zip"
+#download file
+$client.DownloadFile("http://software.intel.com/sites/landingpage/pintool/downloads/pin-2.14-71313-msvc12-windows.zip",$path+"\\pin-2.14-71313-msvc12-windows.zip")
+#extract
+[System.IO.Compression.ZipFile]::ExtractToDirectory([string]$file, [string]$path)
 Start-Sleep -s 2
-$fold=$zm+"\\pin-2.14-71313-msvc12-windows"
-$new =$zm+"\\Pin"
-Rename-Item -path  $fold -newName $new
-$add=$zm+"\\Additional\*"
-$new =$zm+"\\Pin"
+$src=$path+"\\pin-2.14-71313-msvc12-windows"
+$dst =$path+"\\Pin"
+#rename file
+Rename-Item -path  $src -newName $dst
+$add=$path+"\\Additional\*"
+$new =$path+"\\Pin"
 Copy-Item $add $new"\\source\\include\\pin\\"
